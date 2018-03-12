d54342b428a4612ff2fea3a68a9a0198623bd44d
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2018-03-11 17:04:33 -0700

    added local storage functionality

--- content.js
+++ content.js
@@ -4,8 +4,14 @@ var inputs = document.getElementsByTagName('input');
 for (i = 0; i < inputs.length; i++) {
   inputs[i].onkeyup = function() {
     var input = this.value;
-    if (input == "Jeff") { // change to a generalized form!
-      alert("Be careful! You're entering private information.");
-    }
+    var firstname = localStorage.getItem("firstname");
+    var lastname = localStorage.getItem("lastname");
+    var email = localStorage.getItem("email");
+    var mobile = localStorage.getItem("mobile");
+    var address = localStorage.getItem("address");
+    if (input == firstname || input == lastname ||
+      input == email || input == mobile || input == address) {
+        alert("Be careful! You're entering private information into your browser!");
+      }
   };
 }
