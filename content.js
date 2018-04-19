fe310c2b749389d6bcff7260ea390d49745f42a4
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2018-04-19 12:32:21 -0700

    improved UX by removing double-detection on consecutive keyup events

--- content.js
+++ content.js
@@ -1,38 +1,52 @@
-console.log("Chrome extension go");
+console.log("Spidy go!"); // Indicates that Spidy is running.
 
 var address = /^\d+(\s)*[A-z]+(\s)*[A-z]+/;
 var cardnum = /(\d(\s)*){14, 16}/;
 var phonenum = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})/;
 
+var matchstrings = [address, cardnum, phonenum]; // Can be expanded.
+
+localStorage.setItem("lastinput", ""); // Enhances UX by making sure same inputs on separate keyup events aren't double-detected.
+
+/* Function that runs at the start of DOM loading and whenever a mutation on DOM occurs.
+  Checks all input fields on DOM and performs pattern matching to detect whenever sensitive
+  information is being entered. */
 function getInputs() {
   var inputs = document.getElementsByTagName('input');
   for (var i = 0; i < inputs.length; i++) {
     inputs.item(i).onkeyup = function() {
       var input = this.value;
-      var result = checkInputs(input);
-      if (result != "false") {
-        alert("We have detected that you are trying to enter sensitive information. Are you sure you want to enter " + result + " into your browser?");
+      var lastinput = localStorage.getItem("lastinput");
+      if (lastinput != input) {
+        localStorage.setItem("lastinput", input);
+        var result = checkInput(input);
+        if (result) {
+          alert("We have detected that you are trying to enter sensitive information. Are you sure you want to enter this into your browser?");
+        }
       }
-    };
+    }
   }
 }
 
-function checkInputs(input) {
-  if (input.match(address)) {
-    return "an address";
-  }
-  if (input.match(cardnum)) {
-    return "a card number";
-  }
-  if (input.match(phonenum)) {
-    return "this";
-  }
-  else {
-    return "false";
-  }
+/* Function to update points. */
+function updatePoints(currpoints, webpages, domains) {
+  return currpoints + webpages/4 + domains;
 }
-getInputs();
 
+/* Function that checks an input with patterns. */
+function checkInput(input) {
+  for (var i = 0; i < matchstrings.length; i++) {
+    if (input.match(matchstrings[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+getInputs(); // getInputs() executed at the start of DOM loading.
+
+
+/* Detects mutations in DOM to ensure that all input fields in DOM are accounted for. */
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if (mutation.addedNodes && mutation.addedNodes.length > 0) {
