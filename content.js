b35ef24774a6d4154ce5bd20fd73464f28cf66c5
Jun Han Jeff Xiang <jeffxiang@berkeley.edu>
2018-05-01 15:02:48 -0700

    updated method descriptions

--- content.js
+++ content.js
@@ -16,15 +16,17 @@ function getInputs() {
     return inputs;
 }
 
-/* Function that takes a string as an input and gets rid of all whitespaces. */
+/* Function that takes a string as an input and gets rid of all whitespaces.
+    Returns the resultant string. */
 function ridWhitespace(string) {
   return string.replace(/ /g, "");
 }
 
 /* Function that runs at the start of DOM loading and whenever a mutation on DOM occurs.
-  Checks all input fields on DOM and performs pattern matching to detect whenever sensitive
-  information is being entered.
-  Takes in a NodeList INPUTS as parameter. */
+    Checks all input fields on DOM and performs pattern matching to detect whenever sensitive
+    information is being entered.
+    Takes in a NodeList INPUTS as parameter.
+    Alerts user whenever a match is detected. */
 function evalInputs(inputs) {
   for (var i = 0; i < inputs.length; i++) {
     inputs.item(i).onkeyup = function() {
@@ -45,17 +47,21 @@ function evalInputs(inputs) {
   }
 }
 
-/* Function to update points. */
+/* Function to update points. Returns the updated number of points. */
 function updatePoints(currpoints, webpages, domains) {
   return currpoints + webpages/4 + domains; // proportional to # of webpages and domains visited, but with heavier weight on domains.
 }
 
 
 
-/* Function that checks an input with patterns. */
+/* Function that checks an input with patterns.
+    Returns true iff any substring of INPUT matches one of the given patterns. */
 function checkInput(input) {
-  // We realize a double for-loop is not ideal. Perhaps if we had time we would implement a
-  // "trie" (a tree-like data structure for string matching), which has better runtime performance.
+  // We realize that runtime for this method is not ideal. For an n-length input
+  // and k-length list of patterns, the worst-case runtime is O(kn^2). This is
+  // the best we can do at the moment, as we cannot find a way to iterate through
+  // all substrings of INPUT in less than O(n^2) time (it is mathematically impossible
+  // to do so, since there are ~n^2 substirngs in an n-length string).
   for (var i = 0; i < input.length; i++) {
     for (var j = i; j < input.length; j++) {
       var substring = input.substring(i, j+1);
@@ -72,7 +78,8 @@ function checkInput(input) {
 evalInputs(getInputs()); // evalInputs(inputs) executed at the start of DOM loading.
 
 
-/* Detects mutations in DOM to ensure that all input fields in DOM are accounted for. */
+/* Detects mutations in DOM to ensure that all input fields in DOM are accounted for.
+    Calls evalInputs(inputs) again if a mutation is detected. */
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         if (mutation.addedNodes && mutation.addedNodes.length > 0) {
